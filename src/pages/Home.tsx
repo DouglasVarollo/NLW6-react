cd9ae51133ffdc2b40f904a5930d5e736f6933b4
Douglas Varollo <douglasvarollo@gmail.com>
2021-06-21 22:58:48 -0300

    add firebase authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,19 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { auth, firebase } from '../services/firebase';
 
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(function (result) {
+      console.log(result);
+
+      history.push('/rooms/new');
+    });
   }
 
   return (
@@ -27,7 +34,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img alt="Letmeask" src={logoImg} />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img alt="Logo do Google" src={googleIconImg} />
             Crie sua sala com o Google
           </button>
